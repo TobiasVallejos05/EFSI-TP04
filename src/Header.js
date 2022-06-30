783a53ce72b4d4584dce6e517bfb7b77be93b4dc
TOBIAS GONZALO <46701486@ort>
2022-06-30 12:10:14 -0300

    30/06

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return(
+        <nav class="navbar bg-light">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      Bootstrap
+    </a>
+  </div>
+</nav>
+    );
+}
+
+export default Header; 
